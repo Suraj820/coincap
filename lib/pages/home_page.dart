e3242fd99029b1a3ab5bc8a7a61102735e848132
suraj.pal <surajpal.enjay@gmail.com>
2023-07-10 09:34:32 +0530

    Implemented DropDown

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,20 +1,98 @@
+import 'dart:convert';
 
+import 'package:coincap/services/http_service.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 
-class HomePage extends StatefulWidget{
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
   State<StatefulWidget> createState() {
     return _HomePageState();
   }
-
 }
 
 class _HomePageState extends State<HomePage> {
+  double? _deviceHight, _deviceWidth;
+
+  HTTPService? _http;
+
+  @override
+  void initState() {
+    super.initState();
+    _http = GetIt.instance.get<HTTPService>();
+  }
+
   @override
   Widget build(BuildContext context) {
+    _deviceHight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
 
-   return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              _selectedCoinDorpdown(),
+              _dataWidgets(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _selectedCoinDorpdown() {
+    List<String> _coins = ["bitcoin"];
+    List<DropdownMenuItem<String>> _items = _coins
+        .map(
+          (e) => DropdownMenuItem(
+            value: e,
+            child: Text(
+              e,
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 40,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+        )
+        .toList();
+    return DropdownButton(
+      value: _coins.first,
+      items: _items,
+      onChanged: (_value) {},
+      dropdownColor: const Color.fromRGBO(83, 88, 206, 1.0),
+      iconSize: 30,
+      icon: const Icon(
+        Icons.arrow_drop_down_sharp,
+        color: Colors.white,
+      ),
+      underline: Container(),
+    );
+  }
+
+  Widget _dataWidgets() {
+    return FutureBuilder(
+      future: _http!.get("/coins/bitcoin"),
+      builder: (BuildContext _context, AsyncSnapshot _snapshot) {
+        if (_snapshot.hasData) {
+          Map _data =jsonDecode(_snapshot.data.toString());
+          num _usdPrice = _data["market_data"]["current_price"]["usd"];
+          return Text(_usdPrice.toString());
+        } else {
+          return const Center(
+            child: CircularProgressIndicator(
+              color: Colors.white,
+            ),
+          );
+        }
+      },
+    );
   }
 }
