cbc93ea23ce369dc66d404eb3d80f3752b25115d
suraj.pal <surajpal.enjay@gmail.com>
2023-07-11 09:43:32 +0530

    Added Coin Image & details_page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:coincap/pages/details_page.dart';
 import 'package:coincap/services/http_service.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
@@ -82,9 +83,30 @@ class _HomePageState extends State<HomePage> {
       future: _http!.get("/coins/bitcoin"),
       builder: (BuildContext _context, AsyncSnapshot _snapshot) {
         if (_snapshot.hasData) {
-          Map _data =jsonDecode(_snapshot.data.toString());
+          Map _data = jsonDecode(_snapshot.data.toString());
           num _usdPrice = _data["market_data"]["current_price"]["usd"];
-          return Text(_usdPrice.toString());
+          num _change24h = _data["market_data"]["price_change_percentage_24h"];
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              GestureDetector(
+                onDoubleTap: (){
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (BuildContext _context){
+                        return DetailsPage();
+                      }));
+                },
+                child: _coinImageWidget(
+                  _data["image"]["large"],
+                ),
+              ),
+              _currentPriceWidget(_usdPrice),
+              _percentageChangeWidget(_change24h),
+              _descriptionCardWidget(_data["description"]["en"])
+            ],
+          );
         } else {
           return const Center(
             child: CircularProgressIndicator(
@@ -93,6 +115,62 @@ class _HomePageState extends State<HomePage> {
           );
         }
       },
+    );
+  }
+
+  Widget _currentPriceWidget(num _rate) {
+    return Text(
+      "${_rate.toStringAsFixed(2)} USD",
+      style: const TextStyle(
+        color: Colors.white,
+        fontSize: 30,
+        fontWeight: FontWeight.w400,
+      ),
+    );
+  }
+
+  Widget _percentageChangeWidget(num _change) {
+    return Text(
+      "${_change.toString()} %",
+      style: const TextStyle(
+        color: Colors.white,
+        fontSize: 15,
+        fontWeight: FontWeight.w300,
+      ),
+    );
+  }
+
+  Widget _coinImageWidget(String _imgURL) {
+    return Container(
+      padding: EdgeInsets.symmetric(
+        vertical: _deviceHight! * 0.02,
+      ),
+      height: _deviceHight! * 0.15,
+      width: _deviceWidth! * 0.15,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: NetworkImage(_imgURL),
+        ),
+      ),
+    );
+  }
+
+  Widget _descriptionCardWidget(String _description) {
+    return Container(
+      height: _deviceHight! * 0.45,
+      width: _deviceWidth! * 0.90,
+      margin: EdgeInsets.symmetric(
+        vertical: _deviceHight! * 0.05,
+      ),
+      padding: EdgeInsets.symmetric(
+        vertical: _deviceHight! * 0.01,
+        horizontal: _deviceHight! * 0.01,
+      ),
+      color: const Color.fromRGBO(83, 88, 206, 0.5),
+      child: Text(
+        _description,
+        style: const TextStyle(color: Colors.white),
+      ),
     );
   }
 }
